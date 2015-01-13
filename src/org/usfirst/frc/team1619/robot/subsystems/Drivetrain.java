ff00e44de97b6a0e05bb8179b078e45ac916cf0d
Cameron Taleghani <camerontaleghani@gmail.com>
2015-01-12 18:25:01 -0700

    Added RobotDrive object in Drivetrain

--- src/org/usfirst/frc/team1619/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1619/robot/subsystems/Drivetrain.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1619.robot.subsystems;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,6 +10,12 @@ public class Drivetrain extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	private RobotDrive drive;
+	
+	public Drivetrain()
+	{
+		drive = new RobotDrive(1,2,3,4);
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
