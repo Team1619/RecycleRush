a17c4287fcd9d18261e42e9aa644c4b8922bf993
brendonnaftel <brendonnaftel@172.20.10.3>
2015-04-16 15:22:36 -0600

    Changed raker power, driving distance during auto, and commented out robot shake at the end of auto

--- src/org/usfirst/frc/team1619/robot/auto/BinRakerAuto.java
+++ src/org/usfirst/frc/team1619/robot/auto/BinRakerAuto.java
@@ -19,20 +19,20 @@ public class BinRakerAuto extends CommandGroup {
     	
     	addSequential(new WaitCommand(0.5));
     	
-    	addSequential(new LinearDriveCommand(1.0, 1));
-    	addParallel(new ToteElevatorMove(0.5, 0.5));
+    	addSequential(new LinearDriveCommand(2.0, 1));
+    	addParallel(new ToteElevatorMove(2.0, 0.5));
     	
     	addParallel(new MoveBinElevator(false, BinElevatorSystem.kBinElevatorUpSpeed));
     	addSequential(new LinearDriveCommand(2.0, 0.5));
     	addParallel(new RakerMove(RakerSystem.kRakerUpSpeed, 6.0));
-    	for(int i = 0; i < 1; i++) {
-    		addSequential(new LinearDriveCommand(-0.3, 0.7));
-    		addSequential(new LinearDriveCommand(0.3, 0.7));
+//    	for(int i = 0; i < 1; i++) {
+//    		addSequential(new LinearDriveCommand(-0.3, 0.7));
+//    		addSequential(new LinearDriveCommand(0.3, 0.7));
     	}
     	
 //    	addParallel(new LinearDriveCommand(2.5, 0.5));
 //    	addParallel(new RakerMove(RakerSystem.kRakerUpSpeed, 6.0));
 //    	addSequential(new LinearDriveCommand(-0.1, 0.7));
 //    	addSequential(new LinearDriveCommand(0.5, 0.7));
-    }
+//    }
 }
