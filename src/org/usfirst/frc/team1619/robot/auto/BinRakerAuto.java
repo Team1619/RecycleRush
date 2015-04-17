130eb8fdefbc2e5aaeebde5167358b7eb5c44aad
brendonnaftel <brendonnaftel@172.20.10.3>
2015-04-16 20:59:26 -0600

    Robot now moves backwords after raking bins (place holder values)

--- src/org/usfirst/frc/team1619/robot/auto/BinRakerAuto.java
+++ src/org/usfirst/frc/team1619/robot/auto/BinRakerAuto.java
@@ -23,8 +23,9 @@ public class BinRakerAuto extends CommandGroup {
     	addParallel(new ToteElevatorMove(2.0, 0.5));
     	
     	addParallel(new MoveBinElevator(false, BinElevatorSystem.kBinElevatorUpSpeed));
-    	addSequential(new LinearDriveCommand(2.0, 0.5));
+    	addSequential(new LinearDriveCommand(2.2, 0.5));
     	addParallel(new RakerMove(RakerSystem.kRakerUpSpeed, 6.0));
+    	addSequential(new LinearDriveCommand(-0.1, 0.1));
 //    	for(int i = 0; i < 1; i++) {
 //    		addSequential(new LinearDriveCommand(-0.3, 0.7));
 //    		addSequential(new LinearDriveCommand(0.3, 0.7));
